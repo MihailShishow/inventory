dccebbeeb6825553cc6a7efd1afaf780ae53c918
Mihail Shishow <fanoffm@gmail.com>
2019-11-14 17:33:53 +0200

    url fix #3

--- inv_control/urls.py
+++ inv_control/urls.py
@@ -43,7 +43,6 @@ schema_view = get_schema_view(
         license=openapi.License(name="GNU General Public License"),
 
     ),
-    url='/api/',
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
