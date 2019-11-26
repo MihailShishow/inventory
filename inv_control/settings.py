85486d8ede4a81c9bd56a23c6aad70eb6e1289ec
Mihail Shishow <fanoffm@gmail.com>
2019-11-26 20:08:42 +0200

    changed secret_key

--- inv_control/settings.py
+++ inv_control/settings.py
@@ -22,10 +22,11 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e54x2=33tpyf0)xe_4ex4002wdb7g6bcee+2bxh2-jq%tr)1b9'
+# 'e54x2=33tpyf0)xe_4ex4002wdb7g6bcee+2bxh2-jq%tr)1b9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
